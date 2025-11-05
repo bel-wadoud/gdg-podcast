d42090818f805219915e33c02179fd2da7a30dea
boufis <boufisabderrahmane@gmail.com>
2025-11-05 15:29:15 +0100

    add packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,11 @@ import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <>
-      <Landing />
-    </>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
