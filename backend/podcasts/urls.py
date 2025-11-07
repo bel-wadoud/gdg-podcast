c25a581d97e00ed845ec2fe788ee5a09c16efc68
Belheraoui Abdelwadoud <wadoudthedeveloper@gmail.com>
2025-11-07 17:37:20 +0100

    Add route for creating podcast from YouTube

--- backend/podcasts/urls.py
+++ backend/podcasts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path ('podcasts/<int:podcast_id>/download/', views.podcast_download, name = 'download-offline-vedio'),
     path ('user/recommend/',views.podcast_recemonede, name = 'rebommended-feed'),
     path ('categories/', views.category_list, name = 'list-categories'),
-    path ('categories/<int:category_id>/podcasts/', views.podcasts_by_category, name = 'podcasts-by-category'), 
+    path ('categories/<int:category_id>/podcasts/', views.podcasts_by_category, name = 'podcasts-by-category'),
+    path ('podcasts/create-from-youtube/', views.create_podcast_from_youtube, name = 'create-podcast-from-youtube'),
 ]
